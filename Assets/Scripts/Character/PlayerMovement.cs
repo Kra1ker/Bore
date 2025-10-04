f2ade75e51362c5f4c99e31b545668733d6c807d
Kraiker <kirilplaysun@gmail.com>
2025-10-04 22:11:15 +0200

    origin

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -1,61 +1,83 @@
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+namespace BorePlayerMovement
 {
-    [Header("Settings")]
-    private Rigidbody2D rb;
-    public InputActionAsset InputActions;
-    private InputAction IA_moveAction;
 
-    private InputAction IA_jumpAction;
-
-    private Vector2 moveAmount;
-
-    [Header("Parameters")]
-    public float WalkSpeed = 5;
-    public float JumpSpeed = 5;
-    private bool isGrounded;
-
-    private void OnEnable()
+    [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+    public class PlayerMovement : MonoBehaviour
     {
-        InputActions.FindActionMap("Player").Enable();
-    }
-    private void OnDisable()
-    {
-        InputActions.FindActionMap("Player").Disable();
-    }
+        [Header("Settings")]
+        private Rigidbody2D rb;
+        private BoxCollider2D col;
+        private LayerMask layerMask;
 
-    private void Awake()
-    {
-        IA_moveAction = InputSystem.actions.FindAction("Move");
-        IA_jumpAction = InputSystem.actions.FindAction("Jump");
+        public InputActionAsset InputActions;
+        private InputAction IA_moveAction;
+        private InputAction IA_jumpAction;
+        private Vector2 moveAmount;
+        private RaycastHit2D raycastHit2D;
 
-        rb = GetComponent<Rigidbody2D>();
-    }
+        [Header("Parameters")]
+        public float WalkSpeed = 5;
+        public float JumpSpeed = 5;
+        private bool isGrounded;
+        [SerializeField] private float _time;
 
-    private void Update()
-    {
-        moveAmount = IA_moveAction.ReadValue<Vector2>();
-
-        if (IA_jumpAction.WasPressedThisFrame())
+        private void OnEnable()
         {
-            Jump();
+            InputActions.FindActionMap("Player").Enable();
         }
-    }
+        private void OnDisable()
+        {
+            InputActions.FindActionMap("Player").Disable();
+        }
 
-    public void Jump()
-    {
-        rb.AddForceY(JumpSpeed, ForceMode2D.Impulse);
-    }
+        private void Awake()
+        {
+            IA_moveAction = InputSystem.actions.FindAction("Move");
+            IA_jumpAction = InputSystem.actions.FindAction("Jump");
 
-    void FixedUpdate()
-    {
-        Walking();
-    }
+            rb = GetComponent<Rigidbody2D>();
+            col = GetComponent<BoxCollider2D>();
+            layerMask = LayerMask.GetMask("Ground");
+        }
 
-    private void Walking()
-    {
-        rb.linearVelocityX = moveAmount.x * WalkSpeed;
+        private void Update()
+        {
+            moveAmount = IA_moveAction.ReadValue<Vector2>();
+
+            if (IA_jumpAction.WasPressedThisFrame() && isGrounded)
+            {
+                Jump();
+            }
+
+        }
+
+        public void Jump()
+        {
+            rb.AddForceY(JumpSpeed, ForceMode2D.Impulse);
+        }
+
+        void FixedUpdate()
+        {
+            Walking();
+            CheckGrounded();
+            _time += Time.deltaTime;
+        }
+
+        private void Walking()
+        {
+            rb.linearVelocityX = moveAmount.x * WalkSpeed;
+        }
+
+        private void CheckGrounded()
+        {
+            RaycastHit2D rc = Physics2D.Raycast(col.bounds.center, Vector2.down, 2, layerMask);
+            isGrounded = rc.collider != null;
+            Debug.DrawRay(col.bounds.center, Vector2.down * 2, Color.red, 2);
+        }
     }
 }
